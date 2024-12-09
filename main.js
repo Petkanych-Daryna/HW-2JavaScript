4099083de101c8d0972dba6bc790cd00b865468f
Petkanych-Daryna <kavindarina@gmail.com>
2024-12-09 17:27:54 +0200

    JavaScript

---
+++ main.js
@@ -0,0 +1,64 @@
+// -------------------------------------------------------------
+
+let temperatureCelsius = 25;
+let temperatureFahrenheit = (temperatureCelsius * 9/5) + 32;
+console.log(`${temperatureCelsius}°C = ${temperatureFahrenheit}°F`);
+
+// -------------------------------------------------------------
+
+const daysInMonth = 30;
+const hoursInMonth = daysInMonth * 24;
+const minutesInMonth = hoursInMonth * 60;
+console.log(daysInMonth);
+console.log(hoursInMonth);
+console.log(minutesInMonth);
+
+// -------------------------------------------------------------
+
+let health = 100;
+let energy = 50;
+
+health -= 20;
+energy -= 10;
+
+console.log(health);
+console.log(energy);
+
+
+// -------------------------------------------------------------
+
+let total = 250;
+let discount = total * 0.9;
+console.log(discount);
+
+
+// -------------------------------------------------------------
+
+const floatingNumber = 8.65;
+console.log(Math.floor(floatingNumber));
+
+// -------------------------------------------------------------
+
+const stringNumber = "12.36"
+console.log(parseFloat(stringNumber))
+
+// -------------------------------------------------------------
+
+const stringNumberInt = "66"
+console.log(parseInt(stringNumberInt))
+
+// -------------------------------------------------------------
+
+const numderMath = 33;
+console.log(Math.sqrt(numderMath));
+
+
+// -------------------------------------------------------------
+
+const number = 69;
+const numberString = "22";
+
+console.log(parseInt(numberString))
+console.log(number.toString())
+
+// -------------------------------------------------------------
